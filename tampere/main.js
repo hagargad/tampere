372f06efdeb292a2533e4fb1f8f8bce678aa7880
Hagar Hosny <hagar-hosny-gad@hotmail.com>
2019-06-13 17:28:01 +0200

    update(responsiveness)
    updated

---
+++ tampere/main.js
@@ -0,0 +1,46 @@
+/*------------------------------*/
+/* Page Loader
+/*------------------------------*/
+
+
+$(window).load(function() {
+    $('#pageloader').delay(1000).fadeOut('slow');
+});
+/*------------------------------*/
+/* Smooth scroll
+/*------------------------------*/
+
+
+(function($) {
+    "use strict";
+    $(".scroll a[href^='#']").on('click', function(e) {
+        e.preventDefault();
+        var hash = this.hash;
+        $('html, body').stop().animate({
+            scrollTop: $(hash).offset().top
+        }, 2000, 'easeOutExpo');
+    });
+})(jQuery);
+
+$('.collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+
+
+/*------------------------------*/
+/* Scroll to top
+/*------------------------------*/
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        $('.scrollup').fadeIn();
+    } else {
+        $('.scrollup').fadeOut();
+    }
+});
+
+$('.scrollup').click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 2000);
+    return false;
+});
